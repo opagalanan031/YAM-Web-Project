16a1f7d5a58bc99f07fad790ef20e92d704d3364
Oliver Pagalanan <og.pagalanan@yahoo.com>
2022-09-21 15:35:55 -0400

    added login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
-  { path: '/register', component: RegisterComponent },
-  { path: '/login', component: LoginComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }
 
 ];
 
